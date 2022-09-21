2ab11af6258fe517cca57bcda0076645f53e92b5
Xiaoshan Jiang <jasonxiaoshanjiang@gmail.com>
2022-09-21 22:59:41 +1000

    Embedded out templates and added support for parsing embedded templates

--- main.go
+++ main.go
@@ -3,23 +3,23 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"path/filepath"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/xiaoshanjiang/lenslocked/controllers"
+	"github.com/xiaoshanjiang/lenslocked/templates"
 	"github.com/xiaoshanjiang/lenslocked/views"
 )
 
 func main() {
 	r := chi.NewRouter()
 
-	tpl := views.Must(views.Parse(filepath.Join("templates", "home.gohtml")))
+	tpl := views.Must(views.ParseFS(templates.FS, "home.gohtml"))
 	r.Get("/", controllers.StaticHandler(tpl))
 
-	tpl = views.Must(views.Parse(filepath.Join("templates", "contact.gohtml")))
+	tpl = views.Must(views.ParseFS(templates.FS, "contact.gohtml"))
 	r.Get("/contact", controllers.StaticHandler(tpl))
 
-	tpl = views.Must(views.Parse(filepath.Join("templates", "faq.gohtml")))
+	tpl = views.Must(views.ParseFS(templates.FS, "faq.gohtml"))
 	r.Get("/faq", controllers.StaticHandler(tpl))
 
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
