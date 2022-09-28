334df892dfce856c9b269c04406129fd61234603
Xiaoshan Jiang <jasonxiaoshanjiang@gmail.com>
2022-09-28 16:29:43 +1000

    Added the users.Create method to handle the signup form

--- controllers/users.go
+++ controllers/users.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"fmt"
 	"net/http"
 )
 
@@ -12,4 +13,9 @@ type Users struct {
 
 func (u Users) New(w http.ResponseWriter, r *http.Request) {
 	u.Templates.New.Execute(w, nil)
+}
+
+func (u Users) Create(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Email: ", r.FormValue("email"), "\n")
+	fmt.Fprint(w, "Passowrd: ", r.FormValue("password"))
 }
