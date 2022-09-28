273eb1b4272ff12a53bfde5f76e596a7f88cd495
Xiaoshan Jiang <jasonxiaoshanjiang@gmail.com>
2022-09-28 11:37:41 +1000

    created an interface for template and used it in the users and static controller

--- controllers/users.go
+++ controllers/users.go
@@ -2,13 +2,11 @@ package controllers
 
 import (
 	"net/http"
-
-	"github.com/xiaoshanjiang/lenslocked/views"
 )
 
 type Users struct {
 	Templates struct {
-		New views.Template
+		New Template
 	}
 }
 
