3dc3ccf9443f6655dc332d289f48ba61c1496f34
Xiaoshan Jiang <jasonxiaoshanjiang@gmail.com>
2022-10-11 00:22:19 +1100

    Acquire new record id after inserting into table

--- cmd/exp/main.go
+++ cmd/exp/main.go
@@ -17,8 +17,7 @@ type PostgresConfig struct {
 }
 
 func (cfg PostgresConfig) String() string {
-	return fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
-		cfg.Host, cfg.Port, cfg.User, cfg.Password, cfg.Database, cfg.SSLMode)
+	return fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s", cfg.Host, cfg.Port, cfg.User, cfg.Password, cfg.Database, cfg.SSLMode)
 }
 
 func main() {
@@ -43,30 +42,32 @@ func main() {
 
 	fmt.Println("Connected!")
 
-	_, err = db.Exec(`
-	  CREATE TABLE IF NOT EXISTS users (
-		id SERIAL PRIMARY KEY,
-		name TEXT,
-		email TEXT NOT NULL
-	  );
-	  CREATE TABLE IF NOT EXISTS orders (
-		id SERIAL PRIMARY KEY,
-		user_id INT NOT NULL,
-		amount INT,
-		description TEXT
-	  );`)
+	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    name TEXT,
+    email TEXT NOT NULL
+  );
+
+  CREATE TABLE IF NOT EXISTS orders (
+    id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
+    amount INT,
+    description TEXT
+  );`)
 	if err != nil {
 		panic(err)
 	}
 	fmt.Println("Tables created.")
 
-	name := "SpongeBob SquarePants"
-	email := "demo@user.com"
-	_, err = db.Exec(`
-		INSERT INTO users(name, email)
-		VALUES($1, $2);`, name, email)
+	name := "New User"
+	email := "new@example.com"
+	row := db.QueryRow(`
+    INSERT INTO users (name, email)
+    VALUES ($1, $2) RETURNING id;`, name, email)
+	var id int
+	err = row.Scan(&id)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println("User created.")
+	fmt.Println("User created. id =", id)
 }
