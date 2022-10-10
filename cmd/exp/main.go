71d1e4284bbf2de1cac92ae7ebb850d44ea17b97
Xiaoshan Jiang <jasonxiaoshanjiang@gmail.com>
2022-10-11 09:50:10 +1100

    Query multiple records from a table

--- cmd/exp/main.go
+++ cmd/exp/main.go
@@ -42,18 +42,19 @@ func main() {
 
 	fmt.Println("Connected!")
 
-	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS users (
-    id SERIAL PRIMARY KEY,
-    name TEXT,
-    email TEXT UNIQUE NOT NULL
-  );
+	_, err = db.Exec(`
+	CREATE TABLE IF NOT EXISTS users (
+		id SERIAL PRIMARY KEY,
+		name TEXT,
+		email TEXT UNIQUE NOT NULL
+	);
 
-  CREATE TABLE IF NOT EXISTS orders (
-    id SERIAL PRIMARY KEY,
-    user_id INT NOT NULL,
-    amount INT,
-    description TEXT
-  );`)
+	CREATE TABLE IF NOT EXISTS orders (
+		id SERIAL PRIMARY KEY,
+		user_id INT NOT NULL,
+		amount INT,
+		description TEXT
+	);`)
 	if err != nil {
 		panic(err)
 	}
@@ -71,31 +72,64 @@ func main() {
 	// }
 	// fmt.Println("User created. id =", id)
 
-	id := 1 // Choose an ID NOT in your users table
-	row := db.QueryRow(`
-		SELECT name, email
-		FROM users
-		WHERE id=$1;`, id)
-	var name, email string
-	err = row.Scan(&name, &email)
-	if err == sql.ErrNoRows {
-		fmt.Println("Error, no rows!")
+	// id := 1 // Choose an ID NOT in your users table
+	// row := db.QueryRow(`
+	// 	SELECT name, email
+	// 	FROM users
+	// 	WHERE id=$1;`, id)
+	// var name, email string
+	// err = row.Scan(&name, &email)
+	// if err == sql.ErrNoRows {
+	// 	fmt.Println("Error, no rows!")
+	// }
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// fmt.Printf("User information: name=%s, email=%s\n", name, email)
+
+	// userID := 1
+	// for i := 1; i <= 5; i++ {
+	// 	amount := i * 100
+	// 	desc := fmt.Sprintf("Fake order #%d", i)
+	// 	_, err := db.Exec(`
+	// 		INSERT INTO orders(user_id, amount, description)
+	// 		VALUES($1, $2, $3)`, userID, amount, desc)
+	// 	if err != nil {
+	// 		panic(err)
+	// 	}
+	// }
+	// fmt.Println("Created fake orders.")
+
+	type Order struct {
+		ID          int
+		UserID      int
+		Amount      int
+		Description string
 	}
+	var orders []Order
+
+	userID := 1 // Use the same ID you used in the previous lesson
+	rows, err := db.Query(`
+	SELECT id, amount, description
+	FROM orders
+	WHERE user_id=$1`, userID)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("User information: name=%s, email=%s\n", name, email)
-
-	userID := 1
-	for i := 1; i <= 5; i++ {
-		amount := i * 100
-		desc := fmt.Sprintf("Fake order #%d", i)
-		_, err := db.Exec(`
-			INSERT INTO orders(user_id, amount, description)
-			VALUES($1, $2, $3)`, userID, amount, desc)
+	defer rows.Close()
+	for rows.Next() {
+		var order Order
+		order.UserID = userID
+		err := rows.Scan(&order.ID, &order.Amount, &order.Description)
 		if err != nil {
 			panic(err)
 		}
+		orders = append(orders, order)
 	}
-	fmt.Println("Created fake orders.")
+	err = rows.Err()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Orders:", orders)
+
 }
