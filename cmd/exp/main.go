c0e80b8aad8228c6f06cfda8340957389ad9444a
Xiaoshan Jiang <jasonxiaoshanjiang@gmail.com>
2022-10-11 00:45:25 +1100

    Created some fake orders

--- cmd/exp/main.go
+++ cmd/exp/main.go
@@ -85,4 +85,17 @@ func main() {
 		panic(err)
 	}
 	fmt.Printf("User information: name=%s, email=%s\n", name, email)
+
+	userID := 1
+	for i := 1; i <= 5; i++ {
+		amount := i * 100
+		desc := fmt.Sprintf("Fake order #%d", i)
+		_, err := db.Exec(`
+			INSERT INTO orders(user_id, amount, description)
+			VALUES($1, $2, $3)`, userID, amount, desc)
+		if err != nil {
+			panic(err)
+		}
+	}
+	fmt.Println("Created fake orders.")
 }
