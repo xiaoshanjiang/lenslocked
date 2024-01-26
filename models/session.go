6285361642b57154c560413c9ac4adedd4b27fff
xiaoshanjiang <jasonxiaoshanjiang@gmail.com>
2024-01-26 23:35:02 +1100

    updating existing sessions

--- models/session.go
+++ models/session.go
@@ -51,10 +51,24 @@ func (ss *SessionService) Create(userID int) (*Session, error) {
 		TokenHash: ss.hash(token),
 	}
 	row := ss.DB.QueryRow(`
-		INSERT INTO sessions (user_id, token_hash)
-		VALUES ($1, $2)
+		UPDATE sessions
+		SET token_hash = $2
+		WHERE user_id = $1
 		RETURNING id;`, session.UserID, session.TokenHash)
 	err = row.Scan(&session.ID)
+	if err == sql.ErrNoRows {
+		// If no session exists, we will get ErrNoRows. That means we need to
+		// create a session object for that user.
+		row := ss.DB.QueryRow(`
+			INSERT INTO sessions (user_id, token_hash)
+			VALUES ($1, $2)
+			RETURNING id;`, session.UserID, session.TokenHash)
+		// The error will be overwritten with either a new error, or nil
+		err = row.Scan(&session.ID)
+	}
+	// If the err was not sql.ErrNoRows, we need to check to see if it was any
+	// other error. If it was sql.ErrNoRows it will be overwritten inside the if
+	// block, and we still need to check for any errors.
 	if err != nil {
 		return nil, fmt.Errorf("create: %w", err)
 	}
