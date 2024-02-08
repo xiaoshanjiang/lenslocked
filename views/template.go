1cdbe652a2b7c3515124ad3e3b4102b643d582bd
xiaoshanjiang <jasonxiaoshanjiang@gmail.com>
2024-02-08 23:23:10 +1100

    current user template function

--- views/template.go
+++ views/template.go
@@ -10,6 +10,8 @@ import (
 	"net/http"
 
 	"github.com/gorilla/csrf"
+	"github.com/xiaoshanjiang/lenslocked/context"
+	"github.com/xiaoshanjiang/lenslocked/models"
 )
 
 func Must(t Template, err error) Template {
@@ -25,6 +27,9 @@ func ParseFS(fs fs.FS, patterns ...string) (Template, error) {
 		template.FuncMap{
 			"csrfField": func() (template.HTML, error) {
 				return "", fmt.Errorf("csrfField not implemented")
+			},
+			"currentUser": func() (*models.User, error) {
+				return nil, fmt.Errorf("currentUser not implemented")
 			},
 		},
 	)
@@ -62,6 +67,9 @@ func (t Template) Execute(w http.ResponseWriter, r *http.Request, data interface
 		template.FuncMap{
 			"csrfField": func() template.HTML {
 				return csrf.TemplateField(r)
+			},
+			"currentUser": func() *models.User {
+				return context.User(r.Context())
 			},
 		},
 	)
