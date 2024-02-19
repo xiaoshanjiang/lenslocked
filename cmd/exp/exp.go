5d16e4ea1d1a20a0ce4bf5a51bd62d540019a09b
xiaoshanjiang <jasonxiaoshanjiang@gmail.com>
2024-02-19 15:32:21 +1100

    using environemt variables

--- cmd/exp/exp.go
+++ cmd/exp/exp.go
@@ -2,25 +2,35 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"strconv"
 
+	"github.com/joho/godotenv"
 	"github.com/xiaoshanjiang/lenslocked/models"
 )
 
-const (
-	host     = "sandbox.smtp.mailtrap.io"
-	port     = 587
-	username = "f54af7c050e7b2"
-	password = "09f8a9202ff4af"
-)
-
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	host := os.Getenv("SMTP_HOST")
+	portStr := os.Getenv("SMTP_PORT")
+	port, err := strconv.Atoi(portStr)
+	if err != nil {
+		panic(err)
+	}
+	username := os.Getenv("SMTP_USERNAME")
+	password := os.Getenv("SMTP_PASSWORD")
+
 	es := models.NewEmailService(models.SMTPConfig{
 		Host:     host,
 		Port:     port,
 		Username: username,
 		Password: password,
 	})
-	err := es.ForgotPassword("jon@calhoun.io", "https://lenslocked.com/reset-pw?token=abc123")
+	err = es.ForgotPassword("jon@calhoun.io", "https://lenslocked.com/reset-pw?token=abc123")
 	if err != nil {
 		panic(err)
 	}
