6b6ee81e74b6a6cf14dc1773dfd301c7108ec602
xiaoshanjiang <jasonxiaoshanjiang@gmail.com>
2024-02-28 21:26:54 +1100

    fixed gallery routing and csrf bugs

--- main.go
+++ main.go
@@ -106,6 +106,7 @@ func main() {
 	csrfMw := csrf.Protect(
 		[]byte(cfg.CSRF.Key),
 		csrf.Secure(cfg.CSRF.Secure),
+		csrf.Path("/"),
 	)
 
 	// Set up controllers
@@ -169,7 +170,12 @@ func main() {
 		r.Use(umw.RequireUser)
 		r.Get("/", usersC.CurrentUser)
 	})
-	r.Get("/galleries/new", galleriesC.New)
+	r.Route("/galleries", func(r chi.Router) {
+		r.Group(func(r chi.Router) {
+			r.Use(umw.RequireUser)
+			r.Get("/new", galleriesC.New)
+		})
+	})
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page not found", http.StatusNotFound)
 	})
