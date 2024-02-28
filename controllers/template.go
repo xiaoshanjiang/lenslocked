3b8319c39630a603e7c511e9c7ffa6f46e5608b7
xiaoshanjiang <jasonxiaoshanjiang@gmail.com>
2024-02-28 12:07:03 +1100

    accepting errors in templates

--- controllers/template.go
+++ controllers/template.go
@@ -3,5 +3,6 @@ package controllers
 import "net/http"
 
 type Template interface {
-	Execute(w http.ResponseWriter, r *http.Request, data interface{})
+	Execute(w http.ResponseWriter, r *http.Request, data interface{},
+		errs ...error)
 }
--- views/template.go
+++ views/template.go
@@ -32,11 +32,7 @@ func ParseFS(fs fs.FS, patterns ...string) (Template, error) {
 				return nil, fmt.Errorf("currentUser not implemented")
 			},
 			"errors": func() []string {
-				return []string{
-					"Don't do that!",
-					"The email address you provided is already associated with an account.",
-					"Something went wrong.",
-				}
+				return nil
 			},
 		},
 	)
@@ -49,21 +45,12 @@ func ParseFS(fs fs.FS, patterns ...string) (Template, error) {
 	}, nil
 }
 
-// func Parse(filepath string) (Template, error) {
-// 	tpl, err := template.ParseFiles(filepath)
-// 	if err != nil {
-// 		return Template{}, fmt.Errorf("Parsing template: %w", err)
-// 	}
-// 	return Template{
-// 		htmlTpl: tpl,
-// 	}, nil
-// }
-
 type Template struct {
 	htmlTpl *template.Template
 }
 
-func (t Template) Execute(w http.ResponseWriter, r *http.Request, data interface{}) {
+func (t Template) Execute(w http.ResponseWriter, r *http.Request,
+	data interface{}, errs ...error) {
 	tpl, err := t.htmlTpl.Clone()
 	if err != nil {
 		log.Printf("cloning template: %v", err)
@@ -77,6 +64,14 @@ func (t Template) Execute(w http.ResponseWriter, r *http.Request, data interface
 			},
 			"currentUser": func() *models.User {
 				return context.User(r.Context())
+			},
+			"errors": func() []string {
+				var errorMessages []string
+				for _, err := range errs {
+					// TODO: Don't keep this long term - we will see why in a later lesson
+					errorMessages = append(errorMessages, err.Error())
+				}
+				return errorMessages
 			},
 		},
 	)
