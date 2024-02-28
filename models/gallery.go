2e1b5ec5227344bdc8b52a5742ea25dfa0959a36
xiaoshanjiang <jasonxiaoshanjiang@gmail.com>
2024-02-28 16:04:32 +1100

    Querying for Galleries by ID

--- models/gallery.go
+++ models/gallery.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"database/sql"
+	"errors"
 	"fmt"
 )
 
@@ -26,6 +27,24 @@ func (service *GalleryService) Create(title string, userID int) (*Gallery, error
 	err := row.Scan(&gallery.ID)
 	if err != nil {
 		return nil, fmt.Errorf("create gallery: %w", err)
+	}
+	return &gallery, nil
+}
+
+func (service *GalleryService) ByID(id int) (*Gallery, error) {
+	gallery := Gallery{
+		ID: id,
+	}
+	row := service.DB.QueryRow(`
+		SELECT title, user_id
+		FROM galleries
+		WHERE id = $1;`, gallery.ID)
+	err := row.Scan(&gallery.Title, &gallery.UserID)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, ErrNotFound
+		}
+		return nil, fmt.Errorf("query gallery by id: %w", err)
 	}
 	return &gallery, nil
 }
