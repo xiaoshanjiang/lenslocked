099068322c34aec48b44f0efb8ffad6fca4ad5da
xiaoshanjiang <jasonxiaoshanjiang@gmail.com>
2024-03-30 15:54:33 +1100

    Image Upload Handler

--- controllers/galleries.go
+++ controllers/galleries.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"errors"
 	"fmt"
+	"io"
 	"net/http"
 	"net/url"
 	"path/filepath"
@@ -186,6 +187,30 @@ func (g Galleries) Image(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	http.ServeFile(w, r, image.Path)
+}
+
+func (g Galleries) UploadImage(w http.ResponseWriter, r *http.Request) {
+	gallery, err := g.galleryByID(w, r, userMustOwnGallery)
+	if err != nil {
+		return
+	}
+	err = r.ParseMultipartForm(5 << 20) // 5mb
+	if err != nil {
+		http.Error(w, "Something went wrong", http.StatusInternalServerError)
+		return
+	}
+	fileHeaders := r.MultipartForm.File["images"]
+	for _, fileHeader := range fileHeaders {
+		file, err := fileHeader.Open()
+		if err != nil {
+			http.Error(w, "Something went wrong", http.StatusInternalServerError)
+			return
+		}
+		defer file.Close()
+		fmt.Printf("Attempting to upload %v for gallery %d.\n",
+			fileHeader.Filename, gallery.ID)
+		io.Copy(w, file)
+	}
 }
 
 func (g Galleries) DeleteImage(w http.ResponseWriter, r *http.Request) {
--- main.go
+++ main.go
@@ -194,6 +194,7 @@ func main() {
 			r.Post("/{id}", galleriesC.Update)
 			r.Post("/{id}/delete", galleriesC.Delete)
 			r.Post("/{id}/images/{filename}/delete", galleriesC.DeleteImage)
+			r.Post("/{id}/images", galleriesC.UploadImage)
 		})
 	})
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
