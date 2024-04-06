8c7443522505b41b5b8f8900ad28136699b6a9b2
xiaoshanjiang <jasonxiaoshanjiang@gmail.com>
2024-04-06 22:46:50 +1100

    Serving Static Assets

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -202,6 +202,8 @@ func main() {
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page not found", http.StatusNotFound)
 	})
+	assetsHandler := http.FileServer(http.Dir("assets"))
+	r.Get("/assets/*", http.StripPrefix("/assets", assetsHandler).ServeHTTP)
 
 	// Start the server
 	fmt.Printf("Starting the server on %s...\n", cfg.Server.Address)
