e4818fc24728a7716971892e18e01f57b4643747
xiaoshanjiang <jasonxiaoshanjiang@gmail.com>
2024-04-06 22:27:38 +1100

    building Tailwind via Docker

--- tailwind/tailwind.config.js
+++ tailwind/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "../templates/**/*.{gohtml,html}",
+    "/templates/**/*.{gohtml,html}",
   ],
   theme: {
     extend: {},
